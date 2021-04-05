3f2dc01cb4539ccd6852099d04a478a58815693a
SquallX <ribes.alexandre@gmail.com>
2021-04-05 14:11:37 +0200

    Front Page ACF

--- wp-content/themes/webdesign_folio/front-page.php
+++ wp-content/themes/webdesign_folio/front-page.php
@@ -4,9 +4,9 @@
     <header class="masthead bg-primary text-white text-center">
         <div class="container d-flex align-items-center flex-column">
             <!-- Masthead Avatar Image-->
-            <img class="masthead-avatar mb-5" src="<?= get_template_directory_uri(); ?>/assets/img/avataaars.svg" alt="" />
+            <img class="masthead-avatar mb-5" src="<?= the_field('portfolio_image'); ?>" alt="<?= the_field('portfolio_title') ?>" />
             <!-- Masthead Heading-->
-            <h1 class="masthead-heading text-uppercase mb-0">Alexandre Ribes</h1>
+            <h1 class="masthead-heading text-uppercase mb-0"><?= the_field('portfolio_title'); ?></h1>
             <!-- Icon Divider-->
             <div class="divider-custom divider-light">
                 <div class="divider-custom-line"></div>
@@ -14,8 +14,33 @@
                 <div class="divider-custom-line"></div>
             </div>
             <!-- Masthead Subheading-->
-            <p class="masthead-subheading font-weight-light mb-0">Je suis un Web Développer sur Perpignan</p>
+            <?= the_field('portfolio_content'); ?>
         </div>
     </header>
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <?php while (have_posts()) : the_post() ?>
+    <main class="container py-5">
+        <h2 class="text-center"><?= the_title(); ?></h2>
+
+        <div class="row">
+            <div class="col-md-8 offset-4">
+                <?= the_content(); ?>
+            </div>
+        </div>
+
+    </main>
+    <?php endwhile; ?>
 
 <?php get_footer(); ?>
