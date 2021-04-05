adcc4594ab160acd2b8227b133b3352ff3006d5b
SquallX <ribes.alexandre@gmail.com>
2021-04-05 17:28:21 +0200

    Contact Form

--- wp-content/themes/webdesign_folio/front-page.php
+++ wp-content/themes/webdesign_folio/front-page.php
@@ -57,59 +57,27 @@
                 </div>
 
             <?php endwhile; endif; ?>
+            <?php wp_reset_postdata(); ?>
         </section>
 
-
-
-
-
-
-
-        <?php /*
-        <section class="text-center  bg-primary py-5">
-            <?php
-            if (have_rows('recent_works')):
-            while(have_rows('recent_works')): the_row()
-            ?>
-            <h2 class="text-white"><?php the_sub_field('work_title') ?></h2>
-            <p class="text-white"><?php the_sub_field('work_content'); ?></p>
-
+        <section class="page-section" id="contact">
             <div class="container">
+                <!-- Contact Section Heading-->
+                <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0"><?= the_field('contact_title'); ?></h2>
+                <!-- Icon Divider-->
+                <div class="divider-custom">
+                    <div class="divider-custom-line"></div>
+                    <div class="divider-custom-icon"><svg class="svg-inline--fa fa-star fa-w-18" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="star" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" data-fa-i2svg=""><path fill="currentColor" d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg><!-- <i class="fas fa-star"></i> Font Awesome fontawesome.com --></div>
+                    <div class="divider-custom-line"></div>
+                </div>
+                <!-- Contact Section Form-->
                 <div class="row">
-
-                    <?php
-                    $query = new WP_Query(['post_type' => 'work', 'posts_per_page' => 6]);
-                    while($query->have_posts()):
-                        $query->the_post();
-                        ?>
-
-                        <div class="col-md-3 col-lg-3">
-                            <div class="card">
-                                <img class="card-img-top" src="<?= the_post_thumbnail_url(); ?>" alt="<?= the_title(); ?>">
-                                <div class="card-body">
-                                    <h4 class="card-title"><?php the_title(); ?></h4>
-                                    <?php if( has_excerpt()): ?>
-                                        <p class="card-text"><?= the_excerpt(); ?></p>
-                                    <?php endif; ?>
-                                    <a href="<?= the_permalink() ?>" class="btn btn-primary">Découvrir</a>
-                                </div>
-                            </div>
-                        </div>
-
-                    <?php endwhile; ?>
+                    <div class="col-lg-8 mx-auto">
+                        <?= the_field('contact_form'); ?>
+                    </div>
                 </div>
             </div>
-
-
-            <?php
-            endwhile;
-            endif;
-            ?>
-
-
         </section>
- */ ?>
-
     </main>
 
 <?php get_footer(); ?>
