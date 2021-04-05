e280230bca42ebf8b10f15a9a0143cdf36ab41e1
SquallX <ribes.alexandre@gmail.com>
2021-04-05 16:43:19 +0200

    WOrks realisation

--- wp-content/themes/webdesign_folio/front-page.php
+++ wp-content/themes/webdesign_folio/front-page.php
@@ -24,13 +24,55 @@
             <?= the_content(); ?>
         </section>
 
-        <section class="text-center text-white bg-primary py-5">
+        <section class="text-center bg-primary py-5">
+
+            <?php /*
+            Possible également de cette façon si on ne crée pas de sous groupe dans ACF
+            <h2><?= the_field('works_title'); ?></h2>
+            <p><?= the_field('works_content'); ?></p>
+            */ ?>
+
+            <?php
+            if( have_rows('recent_works')):
+                while(have_rows('recent_works')): the_row();
+            ?>
+                <h2><?= the_sub_field('work_title'); ?></h2>
+                <p><?= the_sub_field('work_content'); ?></p>
+
+                <div class="container">
+                    <div class="row">
+                        <?php $query = new WP_Query(['post_type' => 'work', 'posts_per_page' => 6]); ?>
+                        <?php while($query->have_posts()): $query->the_post(); ?>
+                        <div class="col-md-3">
+                            <div class="card">
+                                <img class="card-img-top" src="<?= the_post_thumbnail_url() ?>" />
+                                <div class="card-body">
+                                    <h4><?= the_title() ?></h4>
+                                    <a href="<?= the_permalink(); ?>" class="btn btn-primary" title="Découvrir">Découvrir</a>
+                                </div>
+                            </div>
+                        </div>
+                        <?php endwhile; ?>
+                    </div>
+                </div>
+
+            <?php endwhile; endif; ?>
+        </section>
+
+
+
+
+
+
+
+        <?php /*
+        <section class="text-center  bg-primary py-5">
             <?php
             if (have_rows('recent_works')):
             while(have_rows('recent_works')): the_row()
             ?>
-            <h2><?php the_sub_field('work_title') ?></h2>
-            <p><?php the_sub_field('work_content'); ?></p>
+            <h2 class="text-white"><?php the_sub_field('work_title') ?></h2>
+            <p class="text-white"><?php the_sub_field('work_content'); ?></p>
 
             <div class="container">
                 <div class="row">
@@ -42,8 +84,15 @@
                         ?>
 
                         <div class="col-md-3 col-lg-3">
-                            <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal6">
-                                <img class="img-fluid" src="<?= the_post_thumbnail_url() ?>" alt="" />
+                            <div class="card">
+                                <img class="card-img-top" src="<?= the_post_thumbnail_url(); ?>" alt="<?= the_title(); ?>">
+                                <div class="card-body">
+                                    <h4 class="card-title"><?php the_title(); ?></h4>
+                                    <?php if( has_excerpt()): ?>
+                                        <p class="card-text"><?= the_excerpt(); ?></p>
+                                    <?php endif; ?>
+                                    <a href="<?= the_permalink() ?>" class="btn btn-primary">Découvrir</a>
+                                </div>
                             </div>
                         </div>
 
@@ -59,6 +108,7 @@
 
 
         </section>
+ */ ?>
 
     </main>
 
